0d22bf075e01bb461b16d44a6982e7edf4988e20
Дикунов Михаил Александрович <ma.dikunov@tk.local>
2023-06-16 19:02:27 +0300

    added follows

--- app/models.py
+++ app/models.py
@@ -71,7 +71,16 @@ class Role(db.Model):
             role.default = (role.name == default_role)
             db.session.add(role)
         db.session.commit()
-    
+
+
+class Follow(db.Model):
+    __tablename__ = 'follows'
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                            primary_key = True)
+    followed_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                            primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -88,6 +97,15 @@ class User(UserMixin, db.Model):
     confirmed = db.Column(db.Boolean, default=False)
     avatar_hash = db.Column(db.String(32))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship('Follow', foreign_keys=[Follow.follower_id],
+                                backref=db.backref('follower', lazy='joined'),
+                                lazy='dynamic',
+                                cascade='all, delete-orphan')
+    followers = db.relationship('Follow', foreign_keys=[Follow.followed_id],
+                                backref=db.backref('followed', lazy='joined'),
+                                lazy='dynamic',
+                                cascade='all, delete-orphan')
+
     
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -102,6 +120,11 @@ class User(UserMixin, db.Model):
     @property
     def password(self):
         raise AttributeError('Password is not a readable attribute!')
+    
+    @property
+    def followed_posts(self):
+        return Post.query.join(Follow, Follow.followed_id == Post.author_id).filter(Follow.follower_id == self.id)
+
     
     @password.setter
     def password(self, password):
@@ -152,6 +175,30 @@ class User(UserMixin, db.Model):
                                                                      default=default,
                                                                      rating=rating
                                                                      )
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            f = Follow(follower=self, followed=user)
+            db.session.add(f)
+    
+    def unfollow(self, user):
+        f = self.followed.filter_by(followed_id=user.id).first()
+        if f:
+            db.session.delete(f)
+    
+    def is_following(self, user):
+        if user.id is None:
+            return False
+        return self.followed.filter_by(
+            followed_id=user.id
+        ).first() is not None
+    
+    def is_followed_by(self, user):
+        if user.id is None:
+            return False
+        return self.followers.filter_by(
+            follower_id=user.id
+        ).first is not None
 
 
 class AnonymousUser(AnonymousUserMixin):
--- config.py
+++ config.py
@@ -16,6 +16,7 @@ class Config:
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'admin'
+    FLASKY_FOLLOWERS_PER_PAGE = os.environ.get('FLASKY_FOLLOWERS_PER_PAGE') or 20
     FLASKY_POSTS_PER_PAGE = 20
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
